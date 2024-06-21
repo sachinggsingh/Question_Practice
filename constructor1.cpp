fb6ae5275ce08910cf7ccd73e2f8ca7be461ccb4
Sachin Singh <152975718+sachinggsingh@users.noreply.github.com>
2024-06-21 22:35:03 +0530

    Update constructor1.cpp

--- constructor1.cpp
+++ constructor1.cpp
@@ -1,8 +1,6 @@
 /*Q - 1*/
-/*Write a program to print the names of students by creating a Student class.
-If no name is passed while creating an object of the Student class,
-then the name should be "Unknown", otherwise the name should be equal to 
-the String value passed while creating the object of the Student class.*/
+/*Write a program to print the names of students by creating a Student class. If no name is passed while creating an object of the Student class, then the name should be "Unknown", 
+otherwise the name should be equal to the String value passed while creating the object of the Student class.*/
 #include<iostream>
 using namespace std;
 class Student
